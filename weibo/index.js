fc1dab5fa47032ba5d6867a1b15108133d705241
Juphy <gaozuhui@huayingjuhe.com>
2019-04-19 16:48:40 +0800

    修改user-agent

--- weibo/index.js
+++ weibo/index.js
@@ -160,7 +160,7 @@ class WeiBo {
     // post数据到服务器
     async postData() {
         let headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36",
+            "User-Agent": "Mozilla/5.0 (Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36",
             'Accept-Language': 'zh-cn',
             'Content-Type': 'application/x-www-form-urlencoded',
             'Connection': 'Keep-Alive'
