e24b047a46fd308ec6b21cb5b54f8c027f881cdb
Juphy <gaojuphy@gmail.com>
2019-04-23 00:30:11 +0800

    修改

--- app/nvshens.js
+++ app/nvshens.js
@@ -316,7 +316,7 @@ const init = async () => {
     const tags = await getAllTags(GALLERY);
     let i = 0;
     while (i < tags.length) {
-        let url = NVSHEN + tags[0];
+        let url = NVSHEN + tags[i];
         await main(url, url);
         await new Promise((resolve, reject) => {
             setTimeout(() => {
