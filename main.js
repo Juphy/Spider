0b40a750f4be81ee2fc2aaceea858cd1594ff05d
Juphy <gaozuhui@huayingjuhe.com>
2019-05-10 15:43:09 +0800

    asdqweqwe

--- main.js
+++ main.js
@@ -4,24 +4,3 @@ const WeiBo = require('./weibo/index'),
 let weibo = new WeiBo(config);
 
 module.exports = weibo;
-
-// (async () => {
-//     await weibo.getCookie().catch(async e => {
-//         console.log('cookie error', e);
-//         await weibo.loginto();
-//     })
-//     const promiseArray = urls.map(async item => {
-//         try {
-//             return await weibo.uploadImg(item);
-//         } catch (e) {
-//             weibo.TASK && weibo.TASK.cancel();
-//             await weibo.loginto();
-//         }
-//     })
-
-//     Promise.all(promiseArray).then(async (data) => {
-//         console.log('333', data);
-//     }).catch(async (e) => {
-//         console.log(e);
-//     })
-// })()
